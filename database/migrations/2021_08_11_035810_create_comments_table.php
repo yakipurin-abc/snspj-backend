e72a082d6b52a3936725b25cbdb54746156b0533
yakipurin-abc <k.nishida0425@gmail.com>
2021-08-23 02:25:34 +0900

    remake17

--- database/migrations/2021_08_11_035810_create_comments_table.php
+++ database/migrations/2021_08_11_035810_create_comments_table.php
@@ -16,6 +16,7 @@ class CreateCommentsTable extends Migration
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
             $table->string('user');
+            $table->foreign('user')->references('id')->on('users')->onDelete('cascade');
             $table->string('comment');
             $table->integer('message_id');
             $table->timestamps();
