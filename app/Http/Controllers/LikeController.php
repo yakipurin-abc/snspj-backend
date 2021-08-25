8174c06bdaf90de2f3f742e5086a7f2545ad8525
yakipurin-abc <k.nishida0425@gmail.com>
2021-08-25 18:10:02 +0900

    remake22

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -50,7 +50,7 @@ class LikeController extends Controller
         $likes = Rest::all();
         foreach($likes as $like){
             $item=Like::where('user_id', $request->user_id)->where('rest_id', $request->id)->get();
-            if($item) {
+            if($item.isEmpty()) {
                 $like->isLike=false;
             }else{
                 $like->isLike=true;
